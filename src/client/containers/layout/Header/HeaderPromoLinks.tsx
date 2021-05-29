83b64f778999d2ea6fe7c2425042e3f0b2fa7cef
Mateusz Baginski <cziken58@gmail.com>
2021-05-29 14:10:17 +0200

    more fixes

--- src/client/containers/layout/Header/HeaderPromoLinks.tsx
+++ src/client/containers/layout/Header/HeaderPromoLinks.tsx
@@ -7,20 +7,6 @@ import {useI18n} from '@client/i18n';
 import {CleanList, UndecoratedLink} from '@client/components/ui';
 import {HomeLink} from '@client/routes/Links';
 
-/**
- * @see {@link https://coolors.co/264653-2a9d8f-e9c46a-f4a261-e76f51}
- */
-const PROMO_COLORS = [
-  '#264653',
-  '#2a9d8f',
-  '#e9c46a',
-  '#9ed55b',
-  '#f4a261',
-  '#e76f51',
-  '#1b998b',
-  '#0899ba',
-];
-
 export type HeaderPromoLinksProps = {
   items: {
     icon?: any,
@@ -41,15 +27,12 @@ export const HeaderPromoLinks = ({items}: HeaderPromoLinksProps) => {
       inline
     >
       {visible.map(
-        ({name, href, icon}, index) => (
+        ({name, href, icon}) => (
           <li key={name}>
             <UndecoratedLink
               href={href}
               className='c-header__promo-link'
               activeClassName='is-active'
-              style={{
-                borderBottomColor: PROMO_COLORS[index % PROMO_COLORS.length],
-              }}
             >
               <DynamicIcon
                 icon={icon}
